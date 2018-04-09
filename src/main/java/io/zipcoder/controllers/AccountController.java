62516c814c499d5d1eb0e58892984bbfe59ccae9
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-04-09 11:37:27 -0400

    injected accountservice into accountcontroller

--- src/main/java/io/zipcoder/controllers/AccountController.java
+++ src/main/java/io/zipcoder/controllers/AccountController.java
@@ -1,4 +1,15 @@
 package io.zipcoder.controllers;
 
+import io.zipcoder.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+
+
 }
