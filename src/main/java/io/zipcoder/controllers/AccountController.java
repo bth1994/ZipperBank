78b29f46ecc6fdcd2098ef018a3c19ed94999d31
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-10 19:52:19 -0400

    added one controller

--- src/main/java/io/zipcoder/controllers/AccountController.java
+++ src/main/java/io/zipcoder/controllers/AccountController.java
@@ -2,6 +2,10 @@ package io.zipcoder.controllers;
 
 import io.zipcoder.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +14,9 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
-
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllAccounts(){
+        return accountService.getAllAccounts();
+    }
 
 }
