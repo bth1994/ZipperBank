09d73b677619b10ced8ffe471e0fe1e83a8452fa
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-11 18:45:59 -0400

    deposit controlla and deposit service

--- src/main/java/io/zipcoder/controllers/DepositController.java
+++ src/main/java/io/zipcoder/controllers/DepositController.java
@@ -1,11 +1,47 @@
 package io.zipcoder.controllers;
 
+import io.zipcoder.entities.Deposit;
 import io.zipcoder.services.DepositService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class DepositController {
 
-    @Autowired
     private DepositService depositService;
+
+    @Autowired
+    public DepositController(DepositService depositService) {
+        this.depositService = depositService;
+    }
+
+    @RequestMapping(value = "/accounts/{accountId}/deposits", method = RequestMethod.GET)
+    public ResponseEntity<?> getAllDeposits(@PathVariable Long accountId) {
+        return this.depositService.getAllDeposits(accountId);
+    }
+
+    @RequestMapping(value = "/deposits/{depositId}", method = RequestMethod.GET)
+    public ResponseEntity<?> getDepositById(@PathVariable Long depositId) {
+        return this.depositService.getDepositById(depositId);
+    }
+
+    @RequestMapping(value = "/accounts/{accountId}/deposits", method = RequestMethod.POST)
+    public ResponseEntity<?> createDeposit(@PathVariable Long accountId, @RequestBody Deposit deposit) {
+        return this.depositService.createDeposit(accountId, deposit);
+    }
+
+    @RequestMapping(value = "/deposits/{depositId}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateDeposit(@PathVariable Long depositId, @RequestBody Deposit deposit) {
+        return this.depositService.updateDeposit(depositId, deposit);
+    }
+
+    @RequestMapping(value = "/deposits/{depositId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteDeposit(@PathVariable Long depositId) {
+        return this.depositService.deleteDeposit(depositId);
+    }
+
 
 }
--- src/main/java/io/zipcoder/services/DepositService.java
+++ src/main/java/io/zipcoder/services/DepositService.java
@@ -1,8 +1,12 @@
 package io.zipcoder.services;
 
+import io.zipcoder.entities.Deposit;
 import io.zipcoder.repositories.DepositRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.xml.ws.Response;
 
 @Service
 public class DepositService {
@@ -12,6 +16,26 @@ public class DepositService {
     @Autowired
     public DepositService(DepositRepo depositRepo){
         this.depositRepo = depositRepo;
+    }
+
+    public ResponseEntity<?> getAllDeposits(Long accountId) {
+        return null;
+    }
+
+    public ResponseEntity<?> getDepositById(Long depositId) {
+        return null;
+    }
+
+    public ResponseEntity<?> createDeposit(Long accountId, Deposit deposit) {
+        return null;
+    }
+
+    public ResponseEntity<?> updateDeposit(Long depositId, Deposit deposit) {
+        return null;
+    }
+
+    public ResponseEntity<?> deleteDeposit(Long depositId) {
+        return null;
     }
 
 }
