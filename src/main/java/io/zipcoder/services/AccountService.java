da27cba8a7d410683f9d78976a507b8682d4681c
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-12 20:23:01 -0400

    account services

--- src/main/java/io/zipcoder/services/AccountService.java
+++ src/main/java/io/zipcoder/services/AccountService.java
@@ -3,7 +3,9 @@ package io.zipcoder.services;
 import io.zipcoder.entities.Account;
 import io.zipcoder.entities.Customer;
 import io.zipcoder.repositories.AccountRepo;
+import io.zipcoder.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,28 +20,45 @@ public class AccountService {
     }
 
     public ResponseEntity<Iterable<Account>> getAllAccounts() {
-
-        return null;
+        Iterable<Account> allAccounts = accountRepo.findAll();
+        return new ResponseEntity<>(allAccounts, HttpStatus.OK);
     }
 
     public ResponseEntity<Account> getAccountById(Long accountId) {
-        return null;
+        Account account = accountRepo.findOne(accountId);
+        verifyAccount(accountId);
+        return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
-    public ResponseEntity<Iterable<Account>> getAllAccountsByCustomer(Long customerId) {
+    public ResponseEntity<Iterable<Account>> getAllAccountsByCustomer(Customer customer) {
+        Iterable<Account> accountsContainingId = accountRepo.findAll();
+       while(accountsContainingId.iterator().hasNext()) {
+
+       }
         return null;
     }
 
     public ResponseEntity<Account> createAccount(Customer customer, Long customerId) {
+
         return null;
     }
 
     public ResponseEntity<Account> updateAccount(Long accountId, Account account) {
-        return null;
+        account.setId(accountId);
+        accountRepo.save(account);
+        verifyAccount(accountId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public ResponseEntity deleteAccount(Long accountId) {
         return null;
+    }
+
+    public void verifyAccount(Long accountId) throws ResourceNotFoundException {
+        Account account = accountRepo.findOne(accountId);
+        if(account == null) {
+            throw new ResourceNotFoundException("Account with id " + accountId + " not found");
+        }
     }
 
 }
