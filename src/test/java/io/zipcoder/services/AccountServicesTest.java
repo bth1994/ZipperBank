98815b6448957c4a19c2ef8199168d0a8d8332d8
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-14 18:56:51 -0400

    accountservicesTest

--- src/test/java/io/zipcoder/services/AccountServicesTest.java
+++ src/test/java/io/zipcoder/services/AccountServicesTest.java
@@ -16,10 +16,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.singletonList;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.OK;
 
 
 public class AccountServicesTest {
@@ -29,15 +38,16 @@ public class AccountServicesTest {
     @Mock
     private AccountRepo accountRepo;
 
-    @MockBean
+    @Mock
     private CustomerRepo customerRepo;
 
     @InjectMocks
     private AccountService accountService;
 
     private Account testAccount;
+
     @Before
-    public void init(){
+    public void init() {
         MockitoAnnotations.initMocks(this);
         Customer testCustomer = new Customer();
         testCustomer.setId(1L);
@@ -47,32 +57,61 @@ public class AccountServicesTest {
     }
 
     @Test
-    public void getAllAccountsTest(){
+    public void getAllAccountsTest() {
+        Iterable<Account> accountsList = new ArrayList<>();
+        given(accountRepo.findAll()).willReturn(accountsList);
 
+        ResponseEntity<Iterable<Account>> expected = new ResponseEntity<>(accountsList, OK);
+        ResponseEntity<Iterable<Account>> actual = accountService.getAllAccounts();
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void getAccountsByIdTest(){
+    public void getAccountsByIdTest() {
         accountRepo.save(testAccount);
         when(accountRepo.findOne(1L)).thenReturn(testAccount);
-       ResponseEntity<Account> expected = new ResponseEntity<>(testAccount, HttpStatus.OK);
-       ResponseEntity<Account> actual = accountService.getAccountById(testAccount.getId());
-       Assert.assertEquals(expected, actual);
+        ResponseEntity<Account> expected = new ResponseEntity<>(testAccount, OK);
+        ResponseEntity<Account> actual = accountService.getAccountById(testAccount.getId());
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void getAllAccountsByCustomerIdTest(){
+    public void getAllAccountsByCustomerIdTest() {
+        Iterable<Account> accountsList = new ArrayList<>();
+        accountRepo.save(testAccount);
+        when(accountRepo.findAllAccountsByCustomerId(testAccount.getCustomer().getId())).thenReturn(accountsList);
+
+        ResponseEntity<Iterable<Account>> expected = new ResponseEntity<>(accountsList, OK);
+        ResponseEntity<Iterable<Account>> actual = accountService.getAllAccountsByCustomer(testAccount.getCustomer().getId());
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createAccountTest() {
+        Customer testCust = new Customer();
+        testCust.setId(1L);
+        when(customerRepo.findOne(anyLong())).thenReturn(testCust);
+        when(accountRepo.save(any(Account.class))).thenReturn(testAccount);
+
+        ResponseEntity<Account> expected = new ResponseEntity<>(testAccount, HttpStatus.CREATED);
+        ResponseEntity<Account> actual = accountService.createAccount(testAccount, testAccount.getCustomer().getId());
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void updateAccountTest() {
 
     }
-    @Test
-    public void createAccountTest(){
 
-    }
     @Test
-    public void updateAccountTest(){
+    public void deleteAccountTest() {
+        accountRepo.save(testAccount);
+        accountRepo.delete(1L);
+        when(accountRepo.findOne(1L)).thenReturn(testAccount).thenReturn(null);
 
-    }
-    @Test
-    public void deleteAccountTest(){
-
+        verify(accountRepo, times(1)).delete(1L);
     }
 }
