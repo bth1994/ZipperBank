0a3b0e15a8524a11c223378b90cccadc4bbbe933
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-14 11:39:03 -0400

    forgot

--- src/main/java/io/zipcoder/services/AccountService.java
+++ src/main/java/io/zipcoder/services/AccountService.java
@@ -5,7 +5,6 @@ import io.zipcoder.entities.Customer;
 import io.zipcoder.repositories.AccountRepo;
 import io.zipcoder.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
