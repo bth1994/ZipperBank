6bdfca43163131b2dab54aeaab5474d666af5e8c
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-04-21 12:11:41 -0400

    bill service and bill service test complete

--- src/main/java/io/zipcoder/repositories/BillRepo.java
+++ src/main/java/io/zipcoder/repositories/BillRepo.java
@@ -4,5 +4,9 @@ import io.zipcoder.entities.Bill;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BillRepo extends CrudRepository<Bill, Long> {
+    Iterable<Bill> findAllBillsByAccountId(Long accountId);
+
+    Iterable<Bill> findAllBillsByCustomerId(Long accountId);
+
 
 }
--- src/test/java/io/zipcoder/services/BillServiceTest.java
+++ src/test/java/io/zipcoder/services/BillServiceTest.java
@@ -3,6 +3,7 @@ package io.zipcoder.services;
 import io.zipcoder.entities.Account;
 import io.zipcoder.entities.Bill;
 import io.zipcoder.entities.Customer;
+import io.zipcoder.repositories.AccountRepo;
 import io.zipcoder.repositories.BillRepo;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,14 +13,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static java.util.Collections.singletonList;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class BillServiceTest {
 
-    private MockMvc mockMvc;
     private Bill testBill;
     private Account testAccount;
     private Customer testCustomer;
@@ -27,58 +28,88 @@ public class BillServiceTest {
     @Mock
     private BillRepo billRepo;
 
+    @Mock
+    private AccountRepo accountRepo;
+
     @InjectMocks
     private BillService billService;
 
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
-        testBill = new Bill();
 
+        testCustomer = new Customer();
+        testCustomer.setId(11L);
+
+        testAccount = new Account();
+        testAccount.setId(4L);
+        testAccount.setCustomer(testCustomer);
+
+        testBill = new Bill();
+        testBill.setId(7L);
+        testBill.setAccount(testAccount);
     }
 
     @Test
     public void getBillsByAccountTest() {
-        testBill = new Bill();
-        testBill.setId(7L);
-        billRepo.save(testBill);
         Iterable<Bill> bills = singletonList(testBill);
-
-        when(billRepo.findOne(7L)).thenReturn(testBill);
+        when(billRepo.findAllBillsByAccountId(isA(Long.class))).thenReturn(bills);
 
         ResponseEntity<Iterable<Bill>> expected = new ResponseEntity<>(bills, HttpStatus.OK);
-        ResponseEntity<Iterable<Bill>> actual = billService.getBillsByAccount(testBill.getId());
+        ResponseEntity<Iterable<Bill>> actual = billService.getBillsByAccount(testAccount.getId());
+
+        verify(billRepo).findAllBillsByAccountId(isA(Long.class));
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getBillByBillIdTest() {
-
-
+        when(billRepo.findOne(isA(Long.class))).thenReturn(testBill);
+        ResponseEntity<Bill> expected = new ResponseEntity<>(testBill, HttpStatus.OK);
+        ResponseEntity<Bill> actual = billService.getBillByBillId(testBill.getId());
+        verify(billRepo).findOne(isA(Long.class));
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getBillsByCustomerId() {
-
-
+        Iterable<Bill> bills = singletonList(testBill);
+        when(billRepo.findAllBillsByCustomerId(isA(Long.class))).thenReturn(bills);
+        ResponseEntity<Iterable<Bill>> expected = new ResponseEntity<>(bills, HttpStatus.OK);
+        ResponseEntity<Iterable<Bill>> actual = billService.getBillsByCustomerId(testCustomer.getId());
+        verify(billRepo).findAllBillsByCustomerId(isA(Long.class));
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void createBillTest() {
+        when(accountRepo.findOne(isA(Long.class))).thenReturn(testAccount);
+        when(billRepo.save(isA(Bill.class))).thenReturn(testBill);
 
+        ResponseEntity<Bill> expected = new ResponseEntity<>(testBill, HttpStatus.CREATED);
+        ResponseEntity<Bill> actual = billService.createBill(testAccount.getId(), testBill);
 
+        verify(billRepo).save(isA(Bill.class));
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void updateBillTest() {
+        when(billRepo.save(isA(Bill.class))).thenReturn(testBill);
 
+        ResponseEntity<Bill> expected = new ResponseEntity<>(testBill, HttpStatus.OK);
+        ResponseEntity<Bill> actual = billService.updateBill(testBill.getId(), testBill);
 
+        verify(billRepo).save(isA(Bill.class));
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void deleteBillTest() {
-
-
+        ResponseEntity expected = new ResponseEntity(HttpStatus.OK);
+        ResponseEntity actual = billService.deleteBill(testBill.getId());
+        verify(billRepo).delete(isA(Long.class));
+        Assert.assertEquals(expected, actual);
     }
 
 
