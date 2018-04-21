d98d9fc0b228ee336137c38cee2c77618f49ed0e
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-21 16:44:33 -0400

    deposit

--- src/main/java/io/zipcoder/repositories/WithdrawalRepo.java
+++ src/main/java/io/zipcoder/repositories/WithdrawalRepo.java
@@ -5,17 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WithdrawalRepo extends CrudRepository<Withdrawal, Long> {
-<<<<<<< HEAD
+
     Iterable<Withdrawal> getAllWithdrawalsFromAccId(Long accountId);
     Iterable<Withdrawal> getAllWithdrawalsFromWthDrawId(Long withdrawalId);
-=======
-
     Iterable<Withdrawal> findByAccountId(Long accountId);
 
-<<<<<<< HEAD
-    Withdrawal findByAccount(Long withdrawalId);
->>>>>>> 62ad35effbbf2cb42e4a4644af92f88e2b2ba57b
-=======
-
->>>>>>> 59dba2fec2f89e74988a1b841808219bd2bcf7d7
 }
